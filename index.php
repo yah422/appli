717be0322e6e3156364579b980e1a4dcdfc6911e
SAIDI Asma <saidiasma422@gmail.com>
2024-02-29 14:01:36 +0100

    add btn in index.php and correct the different links in recap.php

--- index.php
+++ index.php
@@ -10,31 +10,49 @@
 
 </head>
 <body>
-    <h1 class="text-primary"> Ajouter un produit </h1>
+    <div style="display: flex; gap:0" class="p-0">
+        <div>
+        <p class="p-4 pb-0 mr-0">
+                <input class="form-control bg-primary text-white text-center" style="width:150px; height:40px;" type="submit" name="submit" value="Ajouter produit">    
+        </p>
+        </div>
+
+        <div>
+        <p class="p-4 pb-0 ml-0">
+                <input class="form-control bg-white text-primary text-center" style="width:80px; height:40px;" type="submit" name="submit" value="Panier">    
+        </p>
+        </div>
+
+    </div>
+
+    <h1 class="text-primary font-weight-bold p-4 pb-0 pt-0 "> Ajouter un produit </h1>
+    <div class="p-5 pt-0 pb-0" >
     <form action="traitement.php" method="post">
         <p>
             <label>
-                Nom du produit:
-                <input type="text" name="name">
+                Nom du produit: <br>
+                <input class=" border-0" type="text" name="name">
             </label>
         </p>
         <p>
             <label>
-                Prix du produit :
-                <input type="number" step="any" name="price">
+                Prix du produit : <br>
+                <input class=" border-0" type="number" step="any" name="price">
             </label>
         </p>
         <p>
             <label>
-                Quantité désirée:
-                <input type="number" name="qtt" value="1">
+                Quantité désirée: <br>
+                <input class=" border-0" type="number" name="qtt" value="1">
             </label>
         </p>
-        <p>
-                <input type="submit" name="submit" value="Ajouter un produit">    
+    </div>
+    <div d-flex align-item-center justify-content-center >
+        <p class="p-4">
+                <input class="form-control bg-primary text-white text-center" style="width:200px; height:40px;" type="submit" name="submit" value="Ajouter le produit">    
         </p>
     </form>
-
+    </div>
     <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.3.3/dist/js/bootstrap.min.js" integrity="sha384-0pUGZvbkm6XF6gxjEnlmuGrJXVbNuzT9qBBavbLwCsOGabYfZo0T0to5eqruptLy" crossorigin="anonymous"></script>
     
 </body>
