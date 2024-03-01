3c236fb7dda81a87488178f27b4bf9a76c85a688
Asma SAIDI <saidiasma422@gmail.com>
2024-03-01 14:51:46 +0100

    add a error message for product in traitement.php

--- traitement.php
+++ traitement.php
@@ -32,6 +32,8 @@ if(isset($_POST['submit'])){
         $_SESSION['products'][]= $product;
         
         $_SESSION['message'] = '<div class="alert alert-success" role="alert"> Votre produit à bien été enregistré ! </div>';
+        } else {
+            $_SESSION['message'] = '<div class="alert alert-danger" role="alert">Votre produit n\'a pas été enregistré !  </div>';
         }
 
 }
@@ -39,7 +41,7 @@ $id = (isset($_GET['id'])) ? $_GET['id'] : null;
 if(isset($_GET['action'])){
 
     switch($_GET['action']){
-        case "ajout": 
+        case "ajout":
             if(isset($_POST['submit'])){
             $name = filter_input(INPUT_POST, "name", FILTER_SANITIZE_STRING);
             $price = filter_input(INPUT_POST, "price", FILTER_VALIDATE_FLOAT, FILTER_FLAG_ALLOW_FRACTION );
@@ -56,10 +58,12 @@ if(isset($_GET['action'])){
         
                 $_SESSION['products'][]= $product;
 
-                $_SESSION['message'] = '<div class="alert alert-success" role="alert"> Votre produit à bien été enregistré ! </div>';
+                $_SESSION['message'] = '<div class="alert alert-success" role="alert">  Votre produit à bien été enregistré !</div>';
                 }
                 break;
     }
+        
+
 }}
 
 header("Location:index.php");
