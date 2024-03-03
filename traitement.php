68e51dd7c206b78b0d8dc41fbd9dc62347904b03
Asma <saidiasma422@gmail.com>
2024-03-03 22:15:54 +0100

    retire the second session_start  in traitement.php

--- traitement.php
+++ traitement.php
@@ -3,9 +3,7 @@
 session_start();
 
 if(isset($_POST['submit'])){
-
-session_start();
-
+    
 if(isset($_POST['submit'])){
     $name = filter_input(INPUT_POST, "name", FILTER_SANITIZE_STRING);
     $price = filter_input(INPUT_POST, "price", FILTER_VALIDATE_FLOAT, FILTER_FLAG_ALLOW_FRACTION );
